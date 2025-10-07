3c983c42b70727f7c5fda69b26ee58090d73dcd8
MeghnaPriyadarshi <priyadarshi.meghna01@gmail.com>
2025-10-07 18:25:58 +0530

    Modified router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import React from 'react'
 import Header from './components/Header'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Router, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import About from './components/About'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import {Router, Routes, Route } from 'react-router-dom'
 
 function Navbar (){
     return(
